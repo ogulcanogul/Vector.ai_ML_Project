9e62e7f8a9ab35b9e25d8a0af780b2041d9833b6
ogulcanogul <40735559+ogulcanogul@users.noreply.github.com>
2021-11-16 09:30:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,8 @@ def predict(file_path):
     test_data = (asarray(test_data))
     test_data = np.expand_dims(test_data, axis=0)
     test_data = tf.image.convert_image_dtype(test_data, dtype=tf.float32)
-
+    img_size = eval(config_params['backbone_models']['input_parameters']['img_size'])
+    test_data = tf.image.resize(test_data, img_size)
     test_class = model_utils.inferenceModel(model_name, paths, test_data)
     test_class = test_class[0].numpy()
     return str(config_params['class_labels'][test_class])
